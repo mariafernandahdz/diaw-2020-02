8a3d055251d2a5e90f687e095f472fe811352016
mariafernandahdz <fernandahdzc@outlook.com>
2021-01-21 23:19:05 -0600

    Configuraciones prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console*/
 //Script de FrontEnd
-console.log('Modern JS Working! Thanks babel...');
+console.log('Modern JS Working! Thanks babel');
 const show = (message = 'Saludo por defecto') => {
-    alert(message)
+  alert(message);
 };
-
-//EJEMPLO 
-show()
+show();
 show('Mi saludo');
