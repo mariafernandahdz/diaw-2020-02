0786678dd2ca7c4792b818f053ca3005a86cc8f0
mariafernandahdz <fernandahdzc@outlook.com>
2021-01-22 00:01:08 -0600

    Se importaron los estilos al index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console*/
 //Script de FrontEnd
+//Importar los estilos
+import './styles/styles.css';
 console.log('Modern JS Working! Thanks babel');
-const show = (message = 'Saludo por defecto') => {
+/*const show = (message = 'Saludo por defecto') => {
   alert(message);
-};
-show();
-show('Mi saludo');
+};*/
