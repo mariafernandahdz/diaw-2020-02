2f97358e89c6ebad1d74b109b2be1550e74ab015
mariafernandahdz <fernandahdzc@outlook.com>
2021-01-22 17:46:54 -0600

    Avance Dropdown

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@
 //Importar los estilos
 //import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
-import 'materialize-css/dist/js/materialize';
+import initDropdown from './helpers/dropdowninit';
+//Ejecutando funcion de inicializacion dropdown
+initDropdown();
