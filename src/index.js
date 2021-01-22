70463866af8c7ea68f3547ceaaa5e7ada57ff24e
mariafernandahdz <fernandahdzc@outlook.com>
2021-01-21 21:35:53 -0600

    Archivos de configuracion y cambios para el airbnb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 //Script de FrontEnd
-console.log("Modern JS Working! Thanks babel...");
-let show = (message = "Saludo por defecto") =>{
+console.log('Modern JS Working! Thanks babel...');
+const show = (message = 'Saludo por defecto') => {
     alert(message)
-}
+};
 
 //EJEMPLO 
 show()
-show("Mi saludo")
+show('Mi saludo');
