c17a249d7516057885245a3a61ba0d489eb82aef
mariafernandahdz <fernandahdzc@outlook.com>
2021-01-22 00:28:26 -0600

    Configuracion de materialize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 /* eslint-disable no-console*/
 //Script de FrontEnd
 //Importar los estilos
-import './styles/styles.css';
+//import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
 console.log('Modern JS Working! Thanks babel');
 /*const show = (message = 'Saludo por defecto') => {
   alert(message);
