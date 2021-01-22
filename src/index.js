677eeefe6479c19b9c68fa6fc5067b96585853be
mariafernandahdz <fernandahdzc@outlook.com>
2021-01-22 00:56:18 -0600

    Se agrego un nuevo script llamado build:client

--- src/index.js
+++ src/index.js
@@ -5,7 +5,3 @@
 //import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
-console.log('Modern JS Working! Thanks babel');
-/*const show = (message = 'Saludo por defecto') => {
-  alert(message);
-};*/
